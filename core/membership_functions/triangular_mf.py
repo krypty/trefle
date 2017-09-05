d2ed5ab5e08f020b598d6a1ea8b24a2f9b72dd1c
Gary Marigliano <gmarigliano93@gmail.com>
2017-09-05 14:38:55 +0200

    Use LinPWMF as a base class for TriangularMF

--- core/membership_functions/triangular_mf.py
+++ core/membership_functions/triangular_mf.py
@@ -1,35 +1,14 @@
-import numpy as np
-
-from core.membership_functions.free_shape_mf import FreeShapeMF
+from core.membership_functions.lin_piece_wise_mf import LinPWMF
 
 
-class TriangularMF(FreeShapeMF):
+class TriangularMF(LinPWMF):
     """
     Assumptions:
     - mf values are bound to [0, 1]
     """
 
     def __init__(self, p_min, p_mid, p_max, n_points=10):
-        n_pts_min_mid = n_points // 2
-        n_pts_mid_max = n_points - n_pts_min_mid
-
-        in_increasing = np.linspace(p_min, p_mid, n_pts_min_mid)
-        in_decreasing = np.linspace(p_mid, p_max, n_pts_mid_max)
-        in_values = np.append(in_increasing, in_decreasing)
-
-        # increasing slope
-        slope = 1 / (p_mid - p_min)
-        out_increasing = [slope * (x - p_min) for x in in_increasing]
-
-        # decreasing slope
-        slope = -1 / (p_max - p_mid)
-        out_decreasing = [slope * (x - p_max) for x in in_decreasing]
-
-        mf_values = np.append(out_increasing, out_decreasing)
-
-        # plt.scatter(in_values, mf_values)
-        # plt.show()
-        super().__init__(in_values, mf_values)
+        super().__init__([p_min, 0], [p_mid, 1], [p_max, 0], n_points=n_points)
 
 
 if __name__ == '__main__':
@@ -39,6 +18,6 @@ if __name__ == '__main__':
 
     # large margin because it is rounded (i.e. limited by the number of points)
     assert abs(mf.fuzzify(2.5) - 0.03846) < 0.1
-    
+
     assert mf.fuzzify(15) == 1
     assert mf.fuzzify(118) == 0
