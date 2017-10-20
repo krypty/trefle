7a62d791ecae012497d2102cf4f3931f58c02d72
Gary Marigliano <gmarigliano93@gmail.com>
2017-10-20 15:06:58 +0200

    Implement tip problem from Octave's fuzzy-logic-toolbox and fix Gaussian

--- core/membership_functions/gaussian_mf.py
+++ core/membership_functions/gaussian_mf.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from core.membership_functions.free_shape_mf import FreeShapeMF
+from view.mf_viewer import MembershipFunctionViewer
 
 
 class GaussianMF(FreeShapeMF):
@@ -17,21 +18,36 @@ class GaussianMF(FreeShapeMF):
     """
 
     def __init__(self, mu, sigma):
-        in_values = []
-        mf_values = []
-        self._mu = mu
-        self._v = sigma ** 2
+        v = sigma ** 2
+
+        # in order to get 99% of the input space,
+        # see https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule
+        in_values = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 25)
+        mf_values = [self.gaussian(i, mu, v) for i in in_values]
+
+        # scale the gaussian to y_max = 1 because Fuzzy sets should be in [0, 1]
+        mf_values_max = max(mf_values)
+        mf_values = [val / mf_values_max for val in mf_values]
+
         super().__init__(in_values, mf_values)
 
-    def fuzzify(self, in_value: float):
-        # gaussian equation,
-        # source: https://en.wikipedia.org/wiki/Normal_distribution
-        return (1.0 / (np.sqrt(2.0 * np.pi * self._v))) * np.exp(
-            -((in_value - self._mu) ** 2) / (2 * self._v))
+    @staticmethod
+    def gaussian(x: float, mu: float, v: float):
+        """
+        Gaussian equation
+        source: https://en.wikipedia.org/wiki/Normal_distribution
+        :param x: value to compute
+        :param mu: mean
+        :param v: variance i.e. sigma*sigma
+        :return: gaussian value of x
+        """
+        return (1.0 / (np.sqrt(2.0 * np.pi * v))) * np.exp(
+            -((x - mu) ** 2) / (2 * v))
 
 
 if __name__ == '__main__':
-    mf = GaussianMF(mu=3, sigma=4)
+    mf = GaussianMF(mu=30, sigma=4)
+    MembershipFunctionViewer(mf).show()
     print(mf.fuzzify(0))
     print(mf.fuzzify(3))
     print(mf.fuzzify(10))
