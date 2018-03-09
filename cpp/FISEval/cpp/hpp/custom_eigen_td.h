87bdb78730fad441aef56ab42687d9ea38022ff1
Gary Marigliano <gmarigliano93@gmail.com>
2018-03-09 13:53:08 +0100

    Add CLOCK_START and CLOCK_STOP macros

--- cpp/FISEval/cpp/hpp/custom_eigen_td.h
+++ cpp/FISEval/cpp/hpp/custom_eigen_td.h
@@ -7,4 +7,8 @@ typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> M
 typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatXf;
 typedef Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatXi;
 
+
+#define CLOCK_START clock_t t0 = clock();
+#define CLOCK_GET double time_in_clock_ticks = clock() - t0;  double time_in_seconds = (double)time_in_clock_ticks / (double)CLOCKS_PER_SEC; cout << "time(ms): " << fixed << time_in_seconds * 1000 << endl;
+
 #endif
