5b1bb234ab447d7145328bb164e09601afa58d50
Gary Marigliano <gmarigliano93@gmail.com>
2018-05-09 10:33:59 +0200

    remove useless main

--- pyfuge/main.py
+++
@@ -1,8 +0,0 @@
-# def main():
-#     print("Please see examples before starting in the examples folder")
-from pyfuge.evo.examples.evo_wine_classifier import run_with_simple_evo
-
-# from evo.examples.evo_iris_classifier import run_with_simple_evo
-
-if __name__ == '__main__':
-    run_with_simple_evo()
