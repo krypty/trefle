c6cceb18eb986ad9bd3b53a3935cea4f1e384737
Gary Marigliano <gmarigliano93@gmail.com>
2018-06-26 15:30:07 +0200

    Put scikit_fuge in proper module

--- pyfuge/evo/skfuge/scikit_fuge.py
+++ pyfuge/evo/skfuge/scikit_fuge.py
@@ -2,10 +2,6 @@ import inspect
 
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.datasets import load_breast_cancer
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.naive_bayes import GaussianNB
 
 from pyfuge.evo.dataset.pf_dataset import PFDataset
 from pyfuge.evo.experiment.base.simple_experiment import SimpleEAExperiment
@@ -13,10 +9,9 @@ from pyfuge.evo.experiment.simple_fis_individual import SimpleFISIndividual
 from pyfuge.evo.fitness_evaluator.pyfuge_fitness_evaluator import \
     PyFUGEFitnessEvaluator
 from pyfuge.evo.helpers.ind_evaluator_utils import IndEvaluatorUtils
-# this will raise KeyError if invalid
 from pyfuge.evo.helpers.native_ind_evaluator import NativeIndEvaluator
-from pyfuge.fs.view.fis_viewer import FISViewer
 
+# this will raise KeyError if invalid
 labels_str_dict = {
     2: ("LOW", "HIGH", "DONT CARE"),
     3: ("LOW", "MEDIUM", "HIGH", "DONT CARE"),
@@ -126,124 +121,8 @@ class FugeClassifier(BaseEstimator, ClassifierMixin):
     @staticmethod
     def compute_labels_weights(dont_care_prob, n_labels):
         other_labels_prob = 1.0 - dont_care_prob
+
         # +1 because sum(prob) = dc_prob + others_prob
         weights = [other_labels_prob / (n_labels + 1)] * n_labels
         weights.append(dont_care_prob)
         return np.array(weights)
-
-
-def main():
-    # Load dataset
-    data = load_breast_cancer()
-    # data = load_iris()
-
-    # Organize our data
-    label_names = data['target_names']
-    labels = data['target']
-    feature_names = data['feature_names']
-    features = data['data']
-
-    def cls_a():
-        # Split our data
-        train, test, train_labels, test_labels = train_test_split(features,
-                                                                  labels,
-                                                                  test_size=0.33)
-        # Initialize our classifier
-        # gnb = GaussianNB()
-        gnb = FugeClassifier(n_rules=3, n_generations=200, pop_size=100)
-
-        # Train our classifier
-        model = gnb.fit(train, train_labels)
-
-        # Make predictions
-        preds = gnb.predict(test)
-        # print(preds)
-
-        # Evaluate accuracy
-        return accuracy_score(test_labels, preds)
-
-    def cls_b():
-        # Split our data
-        train, test, train_labels, test_labels = train_test_split(features,
-                                                                  labels,
-                                                                  test_size=0.33)
-        # Initialize our classifier
-        gnb = GaussianNB()
-        # gnb = FugeClassifier()
-
-        # Train our classifier
-        model = gnb.fit(train, train_labels)
-
-        # Make predictions
-        preds = gnb.predict(test)
-        # print(preds)
-
-        # Evaluate accuracy
-        return accuracy_score(test_labels, preds)
-
-    # fuge_win_count = 0
-    # for _ in range(10):
-    #     a = cls_a()
-    #     b = cls_b()
-    #
-    #     fuge_win_count += int(a > b)
-    # print("FUGE wins {}/10".format(fuge_win_count))
-
-    # Split our data
-    train, test, train_labels, test_labels = train_test_split(features,
-                                                              labels,
-                                                              test_size=0.33)
-    # Initialize our classifier
-    # gnb = GaussianNB()
-    clf = FugeClassifier(n_rules=3, n_generations=200, pop_size=100,
-                         n_labels_per_mf=2, dont_care_prob=0.95, verbose=True)
-
-    # Train our classifier
-    model = clf.fit(train, train_labels)
-
-    # Make predictions
-    preds = clf.predict(test)
-    # print(preds)
-
-    fis = clf.get_best_fuzzy_system()
-
-    print(fis)
-
-    FISViewer(fis).show()
-
-    # Evaluate accuracy
-    print(accuracy_score(test_labels, preds))
-
-
-def gs():
-    # Load dataset
-    data = load_breast_cancer()
-    # data = load_iris()
-
-    # Organize our data
-    label_names = data['target_names']
-    labels = data['target']
-    feature_names = data['feature_names']
-    features = data['data']
-
-    # Split our data
-    train, test, train_labels, test_labels = train_test_split(features,
-                                                              labels,
-                                                              test_size=0.33)
-
-    tuned_params = {"dont_care_prob": [None, 0.1, 0.5, 0.8, 0.99]}
-
-    gs = GridSearchCV(FugeClassifier(n_rules=3, n_generations=200, pop_size=100,
-                                     n_labels_per_mf=3, verbose=True),
-                      tuned_params)
-
-    # for some reason I have to pass y with same shape
-    # otherwise gridsearch throws an error. Not sure why.
-    gs.fit(train, y=train_labels)
-
-    print(gs.best_params_)  # {'intValue': -10} # and that is what we expect :)
-
-
-if __name__ == '__main__':
-    main()
-    # gs()
---
+++ pyfuge/evo/skfuge/use_skfuge.py
@@ -0,0 +1,117 @@
+from sklearn.datasets import load_breast_cancer, load_iris, load_boston
+from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+
+from pyfuge.evo.skfuge.scikit_fuge import FugeClassifier
+from pyfuge.fs.view.fis_viewer import FISViewer
+
+
+def gs():
+    # Load dataset
+    data = load_breast_cancer()
+
+    # Organize our data
+    y_names = data['target_names']
+    y = data['target']
+    X_names = data['feature_names']
+    X = data['data']
+
+    # Split our data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    tuned_params = {"dont_care_prob": [None, 0.1, 0.5, 0.8, 0.99]}
+
+    gs = GridSearchCV(
+        FugeClassifier(n_rules=3, n_generations=100, pop_size=100,
+                       n_labels_per_mf=3, verbose=True),
+        tuned_params)
+
+    gs.fit(X_train, y_train)
+
+    # print the best param value for dont_care_prob
+    print(gs.best_params_)
+
+
+def run_compare():
+    # Load dataset
+    # data = load_breast_cancer()
+    data = load_iris()
+
+    # Organize our data
+    y_names = data['target_names']
+    y = data['target']
+    X_names = data['feature_names']
+    X = data['data']
+
+    # Split our data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    def classify(clf):
+        # Initialize our classifier
+
+        # Train our classifier
+        model = clf.fit(X_train, y_train)
+
+        # Make predictions
+        preds = clf.predict(X_test)
+        # print(preds)
+
+        # Evaluate accuracy
+        return accuracy_score(y_test, preds)
+
+    fuge_win_count = 0
+    n = 5
+    for i in range(n):
+        print("running battle {}...".format(i + 1))
+        a = classify(FugeClassifier(n_rules=3, n_generations=130, pop_size=100))
+        b = classify(KNeighborsClassifier())
+
+        print("FUGE ({:.3f}) vs other ({:.3f})".format(a, b))
+        fuge_win_count += int(a > b)
+    print("")
+    print("FUGE wins {}/{}".format(fuge_win_count, n))
+
+
+def run():
+    # Load dataset
+    data = load_breast_cancer()
+    # data = load_iris()
+
+    # Organize our data
+    y_names = data['target_names']
+    y = data['target']
+    X_names = data['feature_names']
+    X = data['data']
+
+    # FIXME support regression problems
+    # X, y = load_boston(return_X_y=True)
+
+    # Split our data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    # Initialize our classifier
+    clf = FugeClassifier(n_rules=2, n_generations=1000, pop_size=300,
+                         n_labels_per_mf=3, dont_care_prob=0.9, verbose=True)
+
+    # Train our classifier
+    model = clf.fit(X_train, y_train)
+
+    # Make predictions
+    preds = clf.predict(X_test)
+
+    fis = clf.get_best_fuzzy_system()
+
+    print(fis)
+
+    FISViewer(fis).show()
+
+    # Evaluate accuracy
+    print("Simple run score: ")
+    print(accuracy_score(y_test, preds))
+
+
+if __name__ == '__main__':
+    # gs()
+    run()
+    # run_compare()
