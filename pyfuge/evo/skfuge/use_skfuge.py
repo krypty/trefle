ab00515f02148d3298a88cf4c32c55e2566786aa
Gary Marigliano <gmarigliano93@gmail.com>
2018-07-13 16:18:42 +0200

    Add example of GridSearchCV + fitness functions

--- pyfuge/evo/skfuge/use_skfuge.py
+++ pyfuge/evo/skfuge/use_skfuge.py
@@ -1,8 +1,10 @@
 from sklearn.datasets import load_breast_cancer, load_iris
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, make_scorer, recall_score, \
+    classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 
+from pyfuge.evo.skfuge.fitness_functions import basic_fitness_functions
 from pyfuge.evo.skfuge.scikit_fuge import FugeClassifier
 from pyfuge.fs.view.fis_viewer import FISViewer
 
@@ -91,7 +93,7 @@ def run():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
     # Initialize our classifier
-    clf = FugeClassifier(n_rules=2, n_generations=1000, pop_size=300,
+    clf = FugeClassifier(n_rules=2, n_generations=100, pop_size=200,
                          n_labels_per_mf=3, dont_care_prob=0.9, verbose=True)
 
     # Train our classifier
@@ -109,9 +111,52 @@ def run():
     # Evaluate accuracy
     print("Simple run score: ")
     print(accuracy_score(y_test, preds))
+    print(classification_report(y_test, preds))
+
+
+def test_custom_fit_functions_with_gs():
+    import numpy as np
+
+    fit_funcs = basic_fitness_functions.weighted_binary_classif_metrics(
+        sen_w=np.linspace(0, 1, 4),
+        spe_w=1,
+    )
+    print("n of fit funcs", len(fit_funcs))
+    print(fit_funcs)
+
+    # Load dataset
+    data = load_breast_cancer()
+
+    # Organize our data
+    y_names = data['target_names']
+    y = data['target']
+    X_names = data['feature_names']
+    X = data['data']
+
+    # Split our data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    tuned_params = {"fitness_function": fit_funcs}
+
+    # do not use n_jobs > 1
+    gs = GridSearchCV(
+        FugeClassifier(n_rules=3, n_generations=100, pop_size=200,
+                       n_labels_per_mf=3, verbose=True),
+        tuned_params,
+        scoring=make_scorer(recall_score))  # optional
+
+    gs.fit(X_train, y_train)
+
+    # print the best param value for fitness_function arg
+    print(gs.best_params_)
+    best_ff = gs.best_params_["fitness_function"]
+    print(best_ff)
+    print(best_ff.weights()["sen_w"])
+    print(best_ff["sen_w"])
 
 
 if __name__ == '__main__':
     # gs()
-    run()
+    # run()
     # run_compare()
+    test_custom_fit_functions_with_gs()
