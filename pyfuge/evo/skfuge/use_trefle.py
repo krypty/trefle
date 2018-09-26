a5ef1f4bd5749ca94efb600dd214d78448c3f5e8
Gary Marigliano <gmarigliano93@gmail.com>
2018-09-26 14:47:14 +0200

    cleanup use_trefle.py

--- pyfuge/evo/skfuge/use_trefle.py
+++ pyfuge/evo/skfuge/use_trefle.py
@@ -1,80 +1,11 @@
-import numpy as np
-from sklearn.datasets import load_breast_cancer, load_iris, make_classification
-from sklearn.metrics import accuracy_score, mean_squared_error
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-from pyfuge.evo.skfuge.fitness_functions import basic_fitness_functions
-from pyfuge.evo.skfuge.scikit_fuge import FugeClassifier
 from pyfuge.evo.skfuge.trefle_classifier import TrefleClassifier
-from pyfuge.fs.view.fis_viewer import FISViewer
 
 
-# def gs():
-#     # Load dataset
-#     data = load_breast_cancer()
-#
-#     # Organize our data
-#     y_names = data['target_names']
-#     y = data['target']
-#     X_names = data['feature_names']
-#     X = data['data']
-#
-#     # Split our data
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-#
-#     tuned_params = {"dont_care_prob": [None, 0.1, 0.5, 0.8, 0.99]}
-#
-#     gs = GridSearchCV(
-#         FugeClassifier(n_rules=3, n_generations=100, pop_size=100,
-#                        n_labels_per_mf=3, verbose=True),
-#         tuned_params)
-#
-#     gs.fit(X_train, y_train)
-#
-#     # print the best param value for dont_care_prob
-#     print(gs.best_params_)
-
-
-# def run_compare():
-#     # Load dataset
-#     # data = load_breast_cancer()
-#     data = load_iris()
-#
-#     # Organize our data
-#     y_names = data['target_names']
-#     y = data['target']
-#     X_names = data['feature_names']
-#     X = data['data']
-#
-#     # Split our data
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-#
-#     def classify(clf):
-#         # Initialize our classifier
-#
-#         # Train our classifier
-#         model = clf.fit(X_train, y_train)
-#
-#         # Make predictions
-#         preds = clf.predict(X_test)
-#         # print(preds)
-#
-#         # Evaluate accuracy
-#         return accuracy_score(y_test, preds)
-#
-#     fuge_win_count = 0
-#     n = 5
-#     for i in range(n):
-#         print("running battle {}...".format(i + 1))
-#         a = classify(FugeClassifier(n_rules=3, n_generations=130, pop_size=100))
-#         b = classify(KNeighborsClassifier())
-#
-#         print("FUGE ({:.3f}) vs other ({:.3f})".format(a, b))
-#         fuge_win_count += int(a > b)
-#     print("")
-#     print("FUGE wins {}/{}".format(fuge_win_count, n))
-
-
+# @profile(sort="cumulative", filename="/tmp/pyfuge.profile")
 def run():
     import numpy as np
     import random
@@ -96,10 +27,10 @@ def run():
     # FIXME support regression problems
     # X, y = load_boston(return_X_y=True)
 
-    X, y = make_classification(
-        n_samples=500, n_features=10, n_informative=4, n_redundant=2, n_classes=2
-    )
-    y = y.reshape(-1, 1)
+    # X, y = make_classification(
+    #     n_samples=1000, n_features=10,  n_informative=5, n_classes=2
+    # )
+    # y = y.reshape(-1, 1)
 
     # Split our data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
@@ -123,22 +54,24 @@ def run():
     def fit(y_true, y_pred):
         y_pred_thresholded = round_to_cls(y_pred, n_classes=2)
         fitness_val = accuracy_score(y_true, y_pred_thresholded)
-        rmse = 1.0 - mean_squared_error(y_true, y_pred)
-        return rmse + fitness_val
+        # rmse = - mean_squared_error(y_true, y_pred)
+        # return rmse
+        # return rmse + fitness_val
+        return fitness_val
 
     # Initialize our classifier
     clf = TrefleClassifier(
-        n_rules=6,
+        n_rules=5,
         n_classes_per_cons=[2],
         default_cons=[1],
-        n_max_vars_per_rule=8,
-        n_generations=200,
-        pop_size=150,
-        n_labels_per_mf=3,
+        n_max_vars_per_rule=3,
+        n_generations=100,
+        pop_size=60,
+        n_labels_per_mf=2,
         verbose=True,
         dc_weight=1,
-        p_positions_per_lv=16,
-        n_lv_per_ind_sp1=20,
+        # p_positions_per_lv=16,
+        n_lv_per_ind_sp1=40,
         fitness_function=fit,
     )
 
@@ -163,98 +96,5 @@ def run():
     # print(classification_report(y_test, y_pred))
 
 
-def run2():
-    # Load dataset
-    data = load_breast_cancer()
-
-    # Organize our data
-    y_names = data["target_names"]
-    y = data["target"]
-    X_names = data["feature_names"]
-    X = data["data"]
-
-    # Split our data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-
-    fit_func = basic_fitness_functions.weighted_binary_classif_metrics(
-        f1_w=1, acc_w=1, mse_w=0.1
-    )
-    clf = FugeClassifier(
-        n_rules=3,
-        n_generations=100,
-        pop_size=100,
-        dont_care_prob=0.9,
-        n_labels_per_mf=3,
-        fitness_function=fit_func,
-        verbose=True,
-    )
-
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-
-    from sklearn.metrics import f1_score
-
-    score = f1_score(y_true=y_test, y_pred=y_pred)
-    print("score", score)
-    score = accuracy_score(y_true=y_test, y_pred=y_pred)
-    print("score", score)
-
-    fis = clf.get_best_fuzzy_system()
-    FISViewer(fis).show()
-
-
-def test_custom_fit_functions_with_gs():
-    fit_funcs = basic_fitness_functions.weighted_binary_classif_metrics(
-        sen_w=np.linspace(0, 1, 2), spe_w=np.linspace(0, 1, 3)
-    )
-    print("n of fit funcs", len(fit_funcs))
-    print(fit_funcs)
-
-    # Load dataset
-    data = load_breast_cancer()
-
-    # Organize our data
-    y_names = data["target_names"]
-    y = data["target"]
-    X_names = data["feature_names"]
-    X = data["data"]
-
-    # Split our data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-
-    tuned_params = {"fitness_function": fit_funcs}
-
-    # do not use n_jobs > 1
-    from sklearn.metrics import make_scorer
-    from sklearn.metrics import f1_score
-
-    gs = GridSearchCV(
-        FugeClassifier(
-            n_rules=3,
-            n_generations=50,
-            pop_size=50,
-            n_labels_per_mf=3,
-            dont_care_prob=0.9,
-            verbose=True,
-        ),
-        tuned_params,
-        scoring=make_scorer(f1_score),
-    )  # optional
-
-    gs.fit(X_train, y_train)
-
-    # print the best param value for fitness_function arg
-    print(gs.best_params_)
-    best_ff = gs.best_params_["fitness_function"]
-    print(best_ff)
-    print("best sen_w", best_ff["sen_w"])
-    print("best spe_w", best_ff["spe_w"])
-
-
 if __name__ == "__main__":
-    # gs()
     run()
-    # run_compare()
-    # test_custom_fit_functions_with_gs()
-    # run2()
