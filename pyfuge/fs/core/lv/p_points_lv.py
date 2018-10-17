4a631d3d6f8e46142d99c60a26f028fe64edcb93
Gary Marigliano <gmarigliano93@gmail.com>
2018-10-17 10:23:52 +0200

    Extract fuzzy_label_generator and add test

--- pyfuge/fs/core/lv/p_points_lv.py
+++ pyfuge/fs/core/lv/p_points_lv.py
@@ -1,33 +1,7 @@
+from pyfuge.evo.helpers.fuzzy_labels_generator import generate_labels
 from pyfuge.fs.core.lv.linguistic_variable import LinguisticVariable
 from pyfuge.fs.core.mf.lin_piece_wise_mf import LinPWMF
 from pyfuge.fs.view.lv_viewer import LinguisticVariableViewer
-
-
-def generate_labels(n_points):
-    def get_labels(n, prefix, labels=None):
-        if labels is None:
-            _labels = []
-        else:
-            _labels = labels
-
-        # base recursion case
-        if n == -1:
-            return _labels
-
-        # other cases
-        if n > 2:
-            new_label = "{} very {}".format(n, prefix)
-        else:
-            new_label = n * "very " + prefix
-
-        _labels.append(new_label)
-
-        return get_labels(n - 1, prefix, _labels)
-
-    k = n_points - 1  # minus one for the "medium" label
-    k1 = k // 2
-    k = k - k1
-    return [*get_labels(k - 1, "low"), "medium", *reversed(get_labels(k1 - 1, "high"))]
 
 
 class PPointsLV(LinguisticVariable):
@@ -60,20 +34,11 @@ class PPointsLV(LinguisticVariable):
         if p_points != sorted(p_points):
             raise ValueError("p_points must be increasing values")
 
-        if len(p_points) == 2:
-            super(PPointsLV, self).__init__(
-                name,
-                ling_values_dict={
-                    "low": LinPWMF([p_points[0], 1], [p_points[1], 0]),
-                    "high": LinPWMF([p_points[0], 0], [p_points[1], 1]),
-                },
-            )
-        else:
-            labels = generate_labels(len(p_points))
-            mfs = self._create_mfs(p_points)
+        labels = generate_labels(len(p_points))
+        mfs = self._create_mfs(p_points)
 
-            ling_values_dict = {label: lv for label, lv in zip(labels, mfs)}
-            super(PPointsLV, self).__init__(name, ling_values_dict)
+        ling_values_dict = {label: lv for label, lv in zip(labels, mfs)}
+        super(PPointsLV, self).__init__(name, ling_values_dict)
 
     @staticmethod
     def _create_mfs(p_points):
@@ -88,6 +53,6 @@ class PPointsLV(LinguisticVariable):
 if __name__ == "__main__":
 
     name = "yolo"
-    a = [1.1, 4.4, 6.6, 7.7, 8.8]
+    a = [1.1, 4.4, 33]
     lv = PPointsLV(name, a)
     LinguisticVariableViewer(lv).show()
